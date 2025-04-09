755720a17ce950b452ff3af7df6d14b3f4443fe5
Christian Noel Busalanan <156889421+chanbusalanan@users.noreply.github.com>
2025-04-09 13:53:36 +0800

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -8,71 +8,82 @@ Script Purpose:
 ===============================================================================
 */
 
--- Create table silver.crm_cust_info
-drop table if exists silver.crm_cust_info;
-create table silver.crm_cust_info(
-	cst_id			nvarchar(50),
-	cst_key			nvarchar(50),
-	cst_firstname		nvarchar(50),
-	cst_lastname		nvarchar(50),
-	cst_marital_status	nvarchar(50),
-	cst_gender		nvarchar(50),
-	cst_crate_date		date,
-	dwh_create_date		datetime default getdate()
+-- ============================================================================
+-- Table: silver.crm_cust_info
+-- ============================================================================
+DROP TABLE IF EXISTS silver.crm_cust_info;
+CREATE TABLE silver.crm_cust_info (
+    cst_id             NVARCHAR(50),
+    cst_key            NVARCHAR(50),
+    cst_firstname      NVARCHAR(50),
+    cst_lastname       NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gender         NVARCHAR(50),
+    cst_crate_date     DATE,
+    dwh_create_date    DATETIME DEFAULT GETDATE()
 );
 
--- Create table silver.crm_prd_info
-drop table if exists silver.crm_prd_info;
-create table silver.crm_prd_info(
-	prd_id		nvarchar(50),
-	prd_key		nvarchar(50),
-	prd_nm		nvarchar(50),
-	prd_cost	nvarchar(50),
-	prd_line	nvarchar(50),
-	prd_start_dt	date,
-	prd_end_dt	date,
-	dwh_create_date		datetime default getdate()
+-- ============================================================================
+-- Table: silver.crm_prd_info
+-- ============================================================================
+DROP TABLE IF EXISTS silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info (
+    prd_id            NVARCHAR(50),
+    prd_key           NVARCHAR(50),
+    prd_nm            NVARCHAR(50),
+    prd_cost          NVARCHAR(50),
+    prd_line          NVARCHAR(50),
+    prd_start_dt      DATE,
+    prd_end_dt        DATE,
+    dwh_create_date   DATETIME DEFAULT GETDATE()
 );
 
--- Create table silver.crm_sales_details
-drop table if exists silver.crm_sales_details;
-create table silver.crm_sales_details(
-	sls_order_num	nvarchar(50),
-	sls_prd_key	nvarchar(50),
-	sls_cust_id	nvarchar(50),
-	sls_order_dt	nvarchar(50),
-	sls_ship_dt	nvarchar(50),
-	sls_due_dt	nvarchar(50),
-	sls_sales	int,
-	sls_qty		int,
-	sls_price	int,
-	dwh_create_date		datetime default getdate()
+-- ============================================================================
+-- Table: silver.crm_sales_details
+-- ============================================================================
+DROP TABLE IF EXISTS silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details (
+    sls_order_num     NVARCHAR(50),
+    sls_prd_key       NVARCHAR(50),
+    sls_cust_id       NVARCHAR(50),
+    sls_order_dt      NVARCHAR(50),
+    sls_ship_dt       NVARCHAR(50),
+    sls_due_dt        NVARCHAR(50),
+    sls_sales         INT,
+    sls_qty           INT,
+    sls_price         INT,
+    dwh_create_date   DATETIME DEFAULT GETDATE()
 );
 
--- Create table silver.erp_cust_az12
-drop table if exists silver.erp_cust_az12;
-create table silver.erp_cust_az12(
-	cid	nvarchar(50),
-	bdate	date,
-	gen	nvarchar(50),
-	dwh_create_date		datetime default getdate()
-)
+-- ============================================================================
+-- Table: silver.erp_cust_az12
+-- ============================================================================
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12 (
+    cid               NVARCHAR(50),
+    bdate             DATE,
+    gen               NVARCHAR(50),
+    dwh_create_date   DATETIME DEFAULT GETDATE()
+);
 
+-- ============================================================================
+-- Table: silver.erp_loc_a101
+-- ============================================================================
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101 (
+    cid               NVARCHAR(50),
+    cntry             NVARCHAR(50),
+    dwh_create_date   DATETIME DEFAULT GETDATE()
+);
 
--- Create table bronze.erp_loc_a101
-drop table if exists silver.erp_loc_a101;
-create table  silver.erp_loc_a101(
-	cid	nvarchar(50),
-	cntry	nvarchar(50),
-	dwh_create_date		datetime default getdate()
-)
-
--- Create table silver.erp_px_cat_g1v2
-drop table if exists silver.erp_px_cat_g1v2
-create table silver.erp_px_cat_g1v2(
-	id		nvarchar(50),
-	cat		nvarchar(50),
-	subcat		nvarchar(50),
-	maintenance 	nvarchar(50),
-	dwh_create_date		datetime default getdate()
-)
+-- ============================================================================
+-- Table: silver.erp_px_cat_g1v2
+-- ============================================================================
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id                NVARCHAR(50),
+    cat               NVARCHAR(50),
+    subcat            NVARCHAR(50),
+    maintenance       NVARCHAR(50),
+    dwh_create_date   DATETIME DEFAULT GETDATE()
+);
